ebecdd00767244472fda4d3ad598c1cc0801d0e0
Brandon Lien <b1lien@ucsd.edu>
2019-02-28 11:39:12 -0800

    Lab 8 done

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,17 @@
 function initMap() {
 	// add your code here
+	//console.log(L);
+	L.mapquest.key = 'KEY';
+	L.mapquest.key = 'LKYZET9Uj6E91nAZTwKWW73MhOq4Xjdf';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	// Add a marker to the map
+	L.marker([32.88, -117.236]).addTo(map);
 }
